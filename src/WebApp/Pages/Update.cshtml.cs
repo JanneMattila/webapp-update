64698169f62b542d28c28ea0803c81fd9797239c
Janne Mattila <janne.mattila@hotmail.fi>
2021-02-17 14:54:47 +0200

    Add additional parameters

--- src/WebApp/Pages/Update.cshtml.cs
+++ src/WebApp/Pages/Update.cshtml.cs
@@ -8,12 +8,16 @@ namespace WebApp.Pages
         public string MachineName { get; set; }
         public DateTime Started { get; set; }
         public TimeSpan Uptime { get; set; }
+        public string AppEnvironment { get; set; }
+        public string AppEnvironmentSticky { get; set; }
 
         public void OnGet()
         {
             MachineName = Environment.MachineName;
             Started = Program.Started;
             Uptime = DateTime.UtcNow - Started;
+            AppEnvironment = Environment.GetEnvironmentVariable("AppEnvironment");
+            AppEnvironmentSticky = Environment.GetEnvironmentVariable("AppEnvironmentSticky");
         }
     }
 }
