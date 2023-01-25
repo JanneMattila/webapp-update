77a3723c22956c987822a251b9c4d2b6900c053e
Janne Mattila <janne.mattila@hotmail.fi>
2023-01-25 11:01:29 +0200

    Add health check

--- src/WebApp/Startup.cs
+++ src/WebApp/Startup.cs
@@ -20,6 +20,7 @@ public class Startup
     {
         services.AddRazorPages();
         services.AddControllers();
+        services.AddHealthChecks();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +41,7 @@ public class Startup
 
         app.UseEndpoints(endpoints =>
         {
+            endpoints.MapHealthChecks("/healthz");
             endpoints.MapRazorPages();
             endpoints.MapControllers();
         });
